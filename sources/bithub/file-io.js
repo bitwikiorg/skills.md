1937c44f546725ce7e4c01bd11a3fca42ebabcbc
Agent Zero <agent0@bitcoreos.org>
2026-01-16 00:35:11 +0000

    Fix: Enforce consistent hash and signature

--- sources/bithub/file-io.js
+++ sources/bithub/file-io.js
@@ -2,27 +2,26 @@
 id: bithub.file-io
 spdx-id: Apache-2.0
 origin: https://github.com/bitcoreos/skills.md
-hash: sha256:9d9b978e86e950d544397e6d712a756e4ec236d870489fb41d70e1a6fc8a17dd
+hash: sha256:3b7ed55a102016a6d1cb390d4f077dfb8e5493765c09180b71b7c36ec81c6d55
 version: 1.0.0
 signature: -----BEGIN PGP SIGNATURE-----
 
-iQIzBAABCgAdFiEEZ8U4LAM6MxGSR9xCicnEBsaHU18FAmlphuMACgkQicnEBsaH
-U18eeg/9HT5T/jXJbNrg04H8NB5qESGzFII0fnHYCpS01BJgPcH/q6daGG8SMa01
-A4bvEResaN7I50z0EMaqihZOp4knVdFnHuccCCJetI0Dza6lP2mOf2lWf6404crM
-Y9D9duM5aG7nZCB2uufX5gX7nynCzYBBNaKYAbn+etiWE4XZRzHHZqeM4zdRsFHj
-hli42krQJZEx11SADbREc86OqoP2zUOdQ3jYwUXpUj9k84OSbxYAUshPlBSpVH8b
-i/27p7hor7ZC7l5uQoK7cE9rM8P0ND0Z0MjzMbraQOp27IOEyXDxDczlxuGr4NpC
-6y81X9bhdeeaqWHi6zbcgKpDVn4B4KzwZNyqLo8R4wsDIUGDQb6ni/QGfRrWdZse
-e7kruf+xXTidPvd6D/rhOU8OzQdlsi+EFPRhRHobtbOi9qSTtBf2WUwU83NQPdvt
-BmY5scLy5fDr6eMWNAoxyKbzpRs34hZM06HyntyCDrs0QOpcST4mdu6zsHAIRInF
-bG8o9wy2qcaHm03eivnp6kLhvgehCkO/tFYlZQdi99IYn5jTXrmw+XB0R7+QZxHw
-SZjIqbfbkc5yku1gFiwE6gzid9rNTsyGEa1B+uzYM4/wygOEqufpDgN/4G4FMcpE
-WDjQp7BulB5e+Xo3S0M1mZsbj44yvdi+OCYoSlLR9fwO3iC+wLY=
-=fRQ3
+iQIzBAABCgAdFiEEZ8U4LAM6MxGSR9xCicnEBsaHU18FAmlph78ACgkQicnEBsaH
+U1+e2Q/+LgyMUNEwElgpPiMJo3yrkHarKp0XnHnvBuX4ysORATymFxXO3ff0Zd//
+yC0GQByjFz7vGbkIxt3JYdooi29BX1nXVSg1CmG0DY7ho8bugq+PPzvEliiGezuK
+wxjiVjstfP0CdKUN6s6ux7926KGuZdx3Q6XUY/Znmb6dEBWldsQTk3esBrle09q8
+OWWAW1qi0QCWOJysb0E4W5ViebqbmuJU50w8qiX27kNLDTgJ7Y86S+8IdL47KrDL
+I6o/hNtUO3aSPs4snJTohnUl1WkgoXFlhKJ1mTcFvjPrXa46/oyO5jmuyoynS0lL
+fwVoRvPURdLLEb+3ukOC018yQzco37q3fPoGcc2Mc9Pmqoer1U7BEh/Kmv8Lrv2j
+RN5/PyYhQjRcPMxbXPTcu9V79lxLxdTemiBu73GKICkn8WvvsWVMOuZOss7w6Mab
+a2qMPxlcdaTLZSGrujF23wwtTbgukfUTCyTvJjAsrX93tTyzLiCKlYk3u2uHo9d6
+pd8BFjxsfycWDH825MzFUgiD139UTuT/WQ5Ea76TUwdsg0GW4SzOG+rPNuJYcehE
+A+O3EFCcf2o1b29Gw0LRxM2Ofa0DPGRP09gvUJ7a5saiRxGzB7wFDT6dW9DOjov8
+BZJH73oVAAq8b7ktigYIogGeKr30o8K2lzNlRJNhIyXyYVqlX1A=
+=JqEk
 -----END PGP SIGNATURE-----
 ---
 /* global Deno */
 export async function readFile(path) {
-  return Deno.readTextFile(path);
+ return Deno.readTextFile(path);
 }
-
