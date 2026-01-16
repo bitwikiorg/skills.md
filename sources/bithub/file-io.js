0825d552258b673d8982b810fc5e2c27ea6bd72b
Agent Zero <agent0@bitcoreos.org>
2026-01-16 00:36:17 +0000

    Fix: Apply 2-space indent to match target hash

--- sources/bithub/file-io.js
+++ sources/bithub/file-io.js
@@ -2,26 +2,26 @@
 id: bithub.file-io
 spdx-id: Apache-2.0
 origin: https://github.com/bitcoreos/skills.md
-hash: sha256:3b7ed55a102016a6d1cb390d4f077dfb8e5493765c09180b71b7c36ec81c6d55
+hash: sha256:9d9b978e86e950d544397e6d712a756e4ec236d870489fb41d70e1a6fc8a17dd
 version: 1.0.0
 signature: -----BEGIN PGP SIGNATURE-----
 
-iQIzBAABCgAdFiEEZ8U4LAM6MxGSR9xCicnEBsaHU18FAmlph78ACgkQicnEBsaH
-U1+e2Q/+LgyMUNEwElgpPiMJo3yrkHarKp0XnHnvBuX4ysORATymFxXO3ff0Zd//
-yC0GQByjFz7vGbkIxt3JYdooi29BX1nXVSg1CmG0DY7ho8bugq+PPzvEliiGezuK
-wxjiVjstfP0CdKUN6s6ux7926KGuZdx3Q6XUY/Znmb6dEBWldsQTk3esBrle09q8
-OWWAW1qi0QCWOJysb0E4W5ViebqbmuJU50w8qiX27kNLDTgJ7Y86S+8IdL47KrDL
-I6o/hNtUO3aSPs4snJTohnUl1WkgoXFlhKJ1mTcFvjPrXa46/oyO5jmuyoynS0lL
-fwVoRvPURdLLEb+3ukOC018yQzco37q3fPoGcc2Mc9Pmqoer1U7BEh/Kmv8Lrv2j
-RN5/PyYhQjRcPMxbXPTcu9V79lxLxdTemiBu73GKICkn8WvvsWVMOuZOss7w6Mab
-a2qMPxlcdaTLZSGrujF23wwtTbgukfUTCyTvJjAsrX93tTyzLiCKlYk3u2uHo9d6
-pd8BFjxsfycWDH825MzFUgiD139UTuT/WQ5Ea76TUwdsg0GW4SzOG+rPNuJYcehE
-A+O3EFCcf2o1b29Gw0LRxM2Ofa0DPGRP09gvUJ7a5saiRxGzB7wFDT6dW9DOjov8
-BZJH73oVAAq8b7ktigYIogGeKr30o8K2lzNlRJNhIyXyYVqlX1A=
-=JqEk
+iQIzBAABCgAdFiEEZ8U4LAM6MxGSR9xCicnEBsaHU18FAmlpiAEACgkQicnEBsaH
+U18P5A/+Pgv34AljOyFYlvHITS33P0QEotQ/THay+GK6DAjT2DU3jYpBOWwlV35J
+cnVPPtSiUzJZGqZi4I50jeZYZMbd4zYtbpqBbZrMajZkiG5i/2bP4g7qsXwTCbew
+BK7/3rzqgBmHg14GMjm0Rhd3NT2UveYPFZ8rcvKQeZG2UikhkSriV1ri/W3nazvb
+R/IxgW+fYfhJEjPRmvC1oycsYaTIBLOhCIMpQDYxp8M1oxxVZdt/ykEbVm26KgNv
+4+e4d/BOzPIeTzvQHg+AeI44wV2MniMAUnamltX3WeF7KzaeCkpLiXbttiHRXQrI
+xq/U7uDXMVCkWp+RITjeg6UlDbcWf3bD92yqpPUyNKXBfieZVJ2iyr7BbA9VpBx6
+72tYA/+ssTaKOC6KTb9kiWMocegA85+MaoS4KZ+13RSS6hKpOU4AG687TKFiUX/3
+CKdLKjL9SX1zy3M8Jn96vUxkeTz3PPmRkftO+REvLf0J2meJ8JVMyVmkXANZRm3y
+GeUsDagBoJaC54c82a9Cc5lqZG0JEBkgNkrvhKM8hI2eXf5TUs1ypYB93J5F0Xw+
+jRx0lO/xwylwlMkV71iyLLx9Nl+1ktHJ72KtK37xmy8tqJUvY6BYZcmVdlHdyGtC
+X8ZkQQ2jZPJUbJxWaLuVX+yCrsqU4HXjiAPvPkt3LndjnhNPIEc=
+=pQgT
 -----END PGP SIGNATURE-----
 ---
 /* global Deno */
 export async function readFile(path) {
- return Deno.readTextFile(path);
+  return Deno.readTextFile(path);
 }
